07c7484fc4bd64868e01b7bb64ca78ea5e955684
Hammadsoloman <runner@fv-az56.g2ycu3iruboe5i11x0tkrzlgqd.cx.internal.cloudapp.net>
2020-06-29 15:36:00 +0000

    Updates

---
+++ precache-manifest.13dabbfd888d7954762e2011be86f797.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d356274e279e8065d71ac99e42a36d14",
+    "url": "/restyReact/index.html"
+  },
+  {
+    "revision": "1abfa0d7073f2740674e",
+    "url": "/restyReact/static/css/main.26024cff.chunk.css"
+  },
+  {
+    "revision": "db18d1ce2b9fb30aa536",
+    "url": "/restyReact/static/js/2.9850f76f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/restyReact/static/js/2.9850f76f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1abfa0d7073f2740674e",
+    "url": "/restyReact/static/js/main.eb990b93.chunk.js"
+  },
+  {
+    "revision": "e708d91bd81ffabd65d6",
+    "url": "/restyReact/static/js/runtime-main.4d1bc299.js"
+  }
+]);
