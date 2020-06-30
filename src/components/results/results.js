8bb796c17b15ac4589e3c0dc69cdc8f82f40b89f
Hammadsoloman <hammadali95@yahoo.com>
2020-07-01 02:46:00 +0300

    done the lab

---
+++ src/components/results/results.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Results(props) {
+
+
+    return (
+        <div>
+            <div>Count: {props.Count}</div>
+            {
+                <div><pre>Header:{JSON.stringify(props.headers, null, 2)}</pre></div>
+            }
+
+            {
+                <div><pre>{JSON.stringify(props.results, null, 2)}</pre></div>
+            }
+
+
+        </div>
+    );
+
+}
+
+export default Results;
