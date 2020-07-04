e92a497239ce3db7b94597701e30f84c89766ce4
Hammadsoloman <hammadali95@yahoo.com>
2020-07-05 01:38:30 +0300

    fix the problems in the lab

--- src/app.js
+++ src/app.js
@@ -1,42 +1,63 @@
 import React from 'react';
+import ReactJson from 'react-json-view';
+import { Route } from 'react-router-dom';
 
-import './app.scss';
-
-// Let's talk about using index.js and some other name in the component folder
-// There's pros and cons for each way of doing this ...
 import Header from './components/header';
 import Footer from './components/footer';
 import Form from './components/form/form.js';
-import Results from './components/results/results';
+import History from './components/history/history';
+import Loader from './components/loader/loader';
 
-
-
+import './app.scss';
 
 class App extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
+      loading: 'false',
+      header: [],
       results: [],
-      headers: []
-
     };
   }
-  handleForm = (Count, results, headers) => {
-    // this will be called from the Form component on Submit
-    console.log('hi from app', results);
-    this.setState({ Count, results, headers });
-  };
+  toggleLoading = () => {
+    if (this.state.loading === 'false') {
+      let loading = 'true';
+      this.setState({ loading: loading });
+      console.log('this.state.loading: ', loading);
+    } else {
+      setTimeout(() => {
+        let loading = 'false';
+        this.setState({ loading: loading });
+        console.log('this.state.loading: ', loading);
+      }, 500);
+    }
+  }
+  handleForm = (results, header) => {
 
+    this.setState({ results, header });
+  }
+  handelHistory = (method, url, data) => {
+    let bodyData = JSON.stringify(data);
+    this.setState({ method, url, body: { bodyData } });
+  }
 
   render() {
     return (
       <React.Fragment>
         <Header />
+        <Route path="/" exact>
+          <Form title="button text" toggleLoading={this.toggleLoading} handler={this.handleForm} method={this.state.method} url={this.state.url} body={this.state.body} />
+          <Loader loading={this.state.loading} />
+          <h2>Headers:</h2>
+          <ReactJson src={this.state.header} />
+          <h2>Results:</h2>
+          <ReactJson src={this.state.results} />
+        </Route>
+        <Route path="/history" >
+          <History handel={this.handelHistory} />
+        </Route>
         <Footer />
-
-        <Results results={this.state.results} count={this.state.count} headers={this.state.headers} />
-        <Form handler={this.handleForm} />
       </React.Fragment>
     );
   }
--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,65 +1,125 @@
 import React from 'react';
-
-import '../form/form.scss';
+import superagent from 'superagent';
+import './form.scss';
 
 class Form extends React.Component {
 
   constructor(props) {
     super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
       url: '',
       method: '',
-      count: '',
-      results: '',
+      body: '',
       request: {},
     };
+    this.hestoryArray = [];
   }
 
-
-  handleSubmit = async (e) => {
-    //at some point
+  handleSubmit = async e => {
     e.preventDefault();
 
-    let raw = await fetch(this.state.url); // star wars API
-    let results = await raw.json();
-    console.log(raw.headers);
-    let headers = raw.headers;
-    if (results.count) {
-      let Count = results.count;
-      this.props.handler(Count, results, headers);
+    if ((this.state.url && this.state.method) || this.props.url) {
+      let url = '';
+      let method = '';
+      let body = '';
+      let request = {
+        url: this.state.url,
+        method: this.state.method,
+        body: this.state.body,
+      };
+      if (this.state.method === 'post' && this.state.body.data) {
+        body = this.state.body.data;
+
+        await this.postHandler();
+        await this.saveLocalStorage();
+        this.props.toggleLoading();
+      } else if (this.state.method === 'put' && this.state.body.data) {
+        this.props.toggleLoading();
+        await this.postHandler();
+        await this.saveLocalStorage();
+        this.props.toggleLoading();
+      } else if (this.state.method === 'delete') {
+        this.props.toggleLoading();
+        const requestOptionsDelete = {
+          method: 'DELETE',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ body }),
+        };
+        await fetch(this.state.url, requestOptionsDelete);
+        await this.saveLocalStorage();
+        this.props.toggleLoading();
+      } else {
+        this.props.toggleLoading();
+        url = this.state.url || this.props.url;
+        method = this.state.method;
+        superagent.get(url)
+          .then(data => {
+            let people = data.body;
+            let headers = data.headers;
+            this.props.handler(people, headers);
+          });
+        await this.saveLocalStorage();
+        await this.props.toggleLoading();
+      }
+      this.setState({ request, url, method, body });
+    } else {
+
+      alert('missing information');
     }
-    else {
-      let Count = Object.keys(results).length;
-      this.props.handler(Count, results, headers);
+  }
+
+  saveLocalStorage() {
+    let localHistory = JSON.parse(localStorage.getItem('history'));
+    if (localHistory) {
+      this.hestoryArray = localHistory;
     }
-
-    // try {
-    //   const raw = await fetch(this.state.url);
-    //   const results = await raw.json();
-    //   let headers = raw.headers;
-
-    //   if (results.count) {
-    //     let Count = results.count;
-    //     this.props.handler(results, Count, headers);
-    //   }
-    //   else {
-    //     let Count = Object.keys(results).length;
-    //     this.props.handler(results, Count, headers);
-    //   }
-
-
-    // } 
-  };
-
+    let objLocalStorage = { method: this.state.method, url: this.state.url, body: this.state.body };
+    let counter = 0;
+    this.hestoryArray.forEach(val => {
+      if (val.method === objLocalStorage.method && val.url === objLocalStorage.url) {
+        counter = 1;
+      }
+    });
+    if (counter === 0) {
+      this.hestoryArray.push(objLocalStorage);
+      localStorage.setItem('history', JSON.stringify(this.hestoryArray));
+    }
+  }
+  postHandler() {
+    this.props.toggleLoading();
+    const requestOptions = {
+      method: this.state.method.toUpperCase(),
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(this.state.body.data),
+    };
+    fetch(this.state.url, requestOptions)
+      .then(data => this.setState(this.state.body.data));
+    this.props.toggleLoading();
+  }
   handleChangeURL = e => {
+
     const url = e.target.value;
     this.setState({ url });
   };
 
   handleChangeMethod = e => {
+
     const method = e.target.id;
     this.setState({ method });
   };
+
+  bodyHandel = e => {
+
+    const data = JSON.parse(e.target.value);
+    this.setState({ body: { data } });
+  }
 
   render() {
     return (
@@ -67,8 +127,8 @@ class Form extends React.Component {
         <form onSubmit={this.handleSubmit}>
           <label >
             <span>URL: </span>
-            <input name='url' type='text' onChange={this.handleChangeURL} />
-            <button type="submit">GO!</button>
+            <input name='url' type='text' value={this.props.url} onChange={this.handleChangeURL} />
+            <button type="submit">{this.props.title}</button>
           </label>
           <label className="methods">
             <span className={this.state.method === 'get' ? 'active' : ''} id="get" onClick={this.handleChangeMethod}>GET</span>
@@ -76,10 +136,13 @@ class Form extends React.Component {
             <span className={this.state.method === 'put' ? 'active' : ''} id="put" onClick={this.handleChangeMethod}>PUT</span>
             <span className={this.state.method === 'delete' ? 'active' : ''} id="delete" onClick={this.handleChangeMethod}>DELETE</span>
           </label>
+          <textarea name='body' rows="7" cols="50" value={this.props.body} onChange={this.bodyHandel}>
+
+          </textarea>
         </form>
         <section className="results">
-          <span className="method">{this.state.request.method}</span>
-          <span className="url">{this.state.request.url}</span>
+          <span className="method">{this.state.request.method}  {this.props.method}</span>
+          <span className="url">{this.state.request.url} {this.props.url}</span>
         </section>
       </>
     );
